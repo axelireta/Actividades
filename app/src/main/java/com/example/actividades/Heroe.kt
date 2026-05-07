2f320a65bb731bca33f89557b3570a1e54dfa9e0
axelireta <axeliretalizarraga@gmail.com>
2026-05-06 20:30:02 -0600

    Actividad 12 06/05/2026

--- app/src/main/java/com/example/actividades/Heroe.kt
+++ app/src/main/java/com/example/actividades/Heroe.kt
@@ -1,31 +1,7 @@
 package com.example.actividades
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-
-//@Parcelize
-//class Heroe(val nombre: String, val nivel: Int, val vida: Int)
-class Heroe(val nombre: String, val nivel: Int, val vida: Int) : Parcelable {
-
-    override fun describeContents(): Int = 0
-    override fun writeToParcel(dest: Parcel, flags: Int){
-
-        dest.writeString(nombre)
-        dest.writeInt(nivel)
-
-        dest.writeInt(vida)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Heroe>{
-        override fun createFromParcel(parcel: Parcel): Heroe = Heroe(
-            parcel.readString() ?: "",
-            parcel.readInt(),
-            parcel.readInt()
-        )
-        override fun newArray(size: Int): Array<Heroe?> = arrayOfNulls(size)
-    }
-
-
-
-}
+@Parcelize
+class Heroe(val nombre: String, val nivel: Int, val vida: Int) : Parcelable
